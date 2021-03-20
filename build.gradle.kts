a719bb6217fb21ddb629f2bdc9e8e93052a594ae
Jason Cobb <jason.e.cobb@gmail.com>
2021-03-20 12:12:40 -0400

    Add dependency on kotlinx.collections.immutable

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.3")
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
