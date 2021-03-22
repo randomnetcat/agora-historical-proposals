530f993a701666bfa807a14752c43c0c92492605
Jason Cobb <jason.e.cobb@gmail.com>
2021-03-21 22:14:09 -0400

    Add proposal AI

--- src/main/kotlin/org/randomcat/proposal_parser/ProposalData.kt
+++ src/main/kotlin/org/randomcat/proposal_parser/ProposalData.kt
@@ -19,9 +19,17 @@ value class ProposalNumber(val raw: BigInteger) {
     }
 }
 
+@JvmInline
+value class ProposalAI(val raw: BigDecimal) {
+    override fun toString(): String {
+        return raw.toString()
+    }
+}
+
 data class ProposalData(
     val number: ProposalNumber,
     val title: String,
+    val ai: ProposalAI,
     val author: PlayerName,
     val coauthors: ImmutableList<PlayerName>,
     val text: String,
