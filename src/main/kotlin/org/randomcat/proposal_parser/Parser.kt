70304582af16929b5752ab297d211ca6181d31fd
Janet Cobb <git@randomcat.org>
2024-03-12 02:04:25 -0400

    Handle proposal 6681

--- src/main/kotlin/org/randomcat/proposal_parser/Parser.kt
+++ src/main/kotlin/org/randomcat/proposal_parser/Parser.kt
@@ -23,7 +23,8 @@ private fun Message.isDistributionMessage(): Boolean {
             adjustedSubject.startsWith("[Promotor] Super-Emergency Distribution") ||
             adjustedSubject.startsWith("Real Fix Distribution") ||
             adjustedSubject.startsWith("[Promotor] (Corrected) Actual Distribution") ||
-            adjustedSubject.startsWith("[Briefly Promotor] Distribution")
+            adjustedSubject.startsWith("[Briefly Promotor] Distribution") ||
+            adjustedSubject.startsWith("[Promotor] Redistribution")
 }
 
 private val DISTRIBUTION_V0_END_DATE = LocalDate.of(2007, 5, 15)
